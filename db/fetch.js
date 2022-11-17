120952c470318dc46d74340c7905d6a11dd9172e
Bobby2Kills <e.rob93@yahoo.co.uk>
2022-11-17 13:08:20 +0000

    ver0.3-infoFetchWorking

--- db/fetch.js
+++ db/fetch.js
@@ -1,30 +1,75 @@
 const fetch = require('isomorphic-fetch')
 
-
-
-const movieName = //this is where either the movie name, or even better, the IMDB ref would go
-
-
-/*to async await it, it would look like
-const movieName = async(imdbRef){
-    const response = fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${imdbRef},
-    const data = await response.json()
-    console.log(data.title)
-    
-}
-
-movieName(tt0133093)
-*/
-
-
-
-fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${movieName}`, {
+//API Data for movie title, year and ID
+const movieOptions = {
     method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c2ddd54fc7msh6346f81bc272b5ep1a980bjsnee70159cc800',
 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-    }
-})
-.then(response => response.json()) //same on coding rooms, but from https://openlibrary.org/isbn.9780140328721.json
-	.then(response => console.log(response)) // on coding rooms he says .then(data =< console.log(data.title)) - can play around with this.
-	.catch(err => console.error(err));
+    },
+}
+
+//API Data for plot summary
+const plotOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c181a9b90bmsh56c11a4cb18537bp17572djsndda8226f05a7',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
+
+//movieName for API
+// const movieName = "The Matrix" 
+
+
+// tt0133093
+
+
+
+
+// to async await it, it would look like
+// const movieName = async(imdbRef) => {
+//     const response = await fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${imdbRef}`, {
+//         method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c2ddd54fc7msh6346f81bc272b5ep1a980bjsnee70159cc800',
+// 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+//     }
+//     })
+
+// }
+//     const data = await response.json()
+//     console.log(data.title)
+
+
+// movieName(tt0133093)
+
+
+
+// api for finding all movie data
+
+
+const movieName = "Avatar 2009"
+
+// retrieve the movie name and title
+const movieInfo = async(movieName) => {
+    const response = await fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${movieName}`, movieOptions)
+    const data = await response.json()
+    const info = await data.results[0]
+    console.log(info.title, info.year)
+
+}
+
+// retrieve the plot summary for the movie
+const plotSummary = async(movieName)=>{
+    const response = await fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${movieName}`, movieOptions)
+    const data = await response.json()
+    const splitID = data.results[0].id.split("/")[2]
+    
+    const moviePlot = await fetch(`https://online-movie-database.p.rapidapi.com/title/get-plots?tconst=${splitID}`, plotOptions)
+    const plotData = await moviePlot.json()
+    console.log(plotData.plots[0].text)
+
+}
+movieInfo(movieName)
+plotSummary(movieName)
